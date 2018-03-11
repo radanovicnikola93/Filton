35bf5f6bfb50dbfd9793ca4ed2411b5e140e4f28
Nikola <radanovicnikola93@gmail.com>
2018-03-11 13:45:29 +0100

    Added Edit function

--- main.py
+++ main.py
@@ -57,9 +57,28 @@ class GuestbookDetails(BaseHandler):
         params = {'guestbook': guestbook}
         return self.render_template('single_guestbook_entry.html', params=params)
 
+class ModifyGuestBookHandler(BaseHandler):
+    def get(self, guestbook_id):
+        guestbook = GuestBook.get_by_id(int(guestbook_id))
+        params = {'guestbook': guestbook}
+        return self.render_template('modify_guestbook.html', params=params)
+
+    def post(self, guestbook_id):
+        fullname = self.request.get('fullname')
+        email = self.request.get('email')
+        message = self.request.get('message')
+        guestbook = GuestBook.get_by_id(int(guestbook_id))
+        guestbook.fullname = fullname
+        guestbook.email = email
+        guestbook.message = message
+        guestbook.put()
+        return self.redirect_to('guestbook')
+
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/rezultat', RezultatHandler),
     webapp2.Route('/guestbook', GuestBookHandler),
     webapp2.Route('/guestbook/<guestbook_id:\d+>', GuestbookDetails),
+    webapp2.Route('/guestbook/<guestbook_id:\d+>/modify', ModifyGuestBookHandler),
+    webapp2.Route('/guestbook/', GuestBookHandler, name='guestbook'),
 ], debug=True)
